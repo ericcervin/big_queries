1a2d215f2ed31bec9b612f1a6b5c0aa54236763f
Eric Ervin <ericcervin@gmail.com>
2025-11-05 00:14:03 -0500

    added 2015_tree_census_blockface_516196.sql

---
+++ 2015_tree_census_blockface_516196.sql
@@ -0,0 +1,13 @@
+SELECT 
+tree_id, 
+block_id, 
+stump_diam, 
+address, 
+zipcode, 
+nta_name, 
+latitude, 
+longitude,
+spc_latin,
+spc_common 
+FROM `bigquery-public-data.new_york_trees.tree_census_2015` ts
+where block_id = 516196
